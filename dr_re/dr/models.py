97a5fc4251b5cd46b0f515c29e33d70d5a7bb193
SolomonBekele <solubman28@gmial.com>
2023-04-23 11:32:47 -0700

    model

--- dr_re/dr/models.py
+++ dr_re/dr/models.py
@@ -13,16 +13,16 @@ GENDER =(
 
 class Data(models.Model):
     
-    main_features =('yellowing_of_eyes', 'itching', 'abdominal_pain', 'joint_pain',
-                  'abnormal_menstruation', 'muscle_weakness', 'skin_rash', 'neck_pain',
-                   'nausea', 'chills', 'sweating', 'altered_sensorium',
-                   'blurred_and_distorted_vision', 'continuous_feel_of_urine', 'yellowish_skin', 'irritation_in_anus',
-                   'indigestion', 'prominent_veins_on_calf', 'mild_fever', 'lack_of_concentration')
+    main_features =((0,"high_fever"), (1,"nausea"), (2,"skin_rash"), (3,"yellowish_skin"), 
+                    (4,"itching"), (5,"mild_fever"), (6,"vomiting"), (7,"fatigue"),
+                    (8,"abnormal_menstruation"),(9,"muscle_weakness"),(10,"indigestion"),
+                    (11,"joint_pain"),(12,"yellowing_of_eyes"),(13,"headache"),(14,"blurred_and_distorted_vision"),
+                    (15,"continuous_feel_of_urine"),(16,"prominent_veins_on_calf"),(17,"sweating"), 
+                    (18,"altered_sensorium"),(19,lack_of_concentration)', '(20,neck_pain)', '(21,irritation_in_anus)', '(22,abdominal_pain)', '(23,chills)']
     
-    main_features=()
     name = models.CharField(max_length=100,null=True)
     age = models.PositiveIntegerField(validators=[MinLengthValidator(13),MaxLengthValidator(19)])
-    sex = models.PositiveIntegerField(choices=GENDER,null=True)
+    sex = models.PositiveIntegerField(choices=main_features,null=True)
     predictions = models.CharField(max_length=100,blank=True)
     date = models.DateTimeField(auto_now_add=True)
     
