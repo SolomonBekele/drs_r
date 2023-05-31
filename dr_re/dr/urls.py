a6092bef2ca76c25d70926819f201be835a7bc8c
SolomonBekele <solubman28@gmial.com>
2023-05-31 11:59:26 -0700

    sol

--- dr_re/dr/urls.py
+++ dr_re/dr/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("interview3",interview3,name="interview3"),
     path("interview4",interview4,name="interview4"),
     path("vom_interview",vom_interview,name="vom_interview"),
+    path('cough_interview1',cough_interview1,name="cough_interview1"),
     path("recommend",recommend,name='recommend'),
 ]
